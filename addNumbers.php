383bfae0520c80007a297d2c5f207725913cdc01
peaceminusdump <130890134+peaceminusdump@users.noreply.github.com>
2025-10-03 17:27:28 +0800

    Done

---
+++ addNumbers.php
@@ -0,0 +1,11 @@
+<?php
+
+    function addNumbers($a, $b){
+        $sum = $a + $b;
+        echo "The sum of $a and $b is: $sum<br>";
+    }
+
+    echo "The sum of 5 and 10 is: ". (5 + 10) . "<br>";
+
+
+?>
